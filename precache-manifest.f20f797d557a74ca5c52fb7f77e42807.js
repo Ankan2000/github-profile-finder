6eca0b972b31d65f63fbaf01a00bf088fa633948
Ankan <ankanbag1006@gmail.com>
2020-07-06 20:10:08 +0530

    Updates

---
+++ precache-manifest.f20f797d557a74ca5c52fb7f77e42807.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11391e58abdf5edea928515cdf685412",
+    "url": "/github-profile-finder/index.html"
+  },
+  {
+    "revision": "e9a6251c78be330f6ae5",
+    "url": "/github-profile-finder/static/js/2.9363ef70.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-profile-finder/static/js/2.9363ef70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5262920398cdbf7c0cf",
+    "url": "/github-profile-finder/static/js/main.e4064801.chunk.js"
+  },
+  {
+    "revision": "ac6c57316cdc4168d2ce",
+    "url": "/github-profile-finder/static/js/runtime-main.29e5d33c.js"
+  }
+]);
